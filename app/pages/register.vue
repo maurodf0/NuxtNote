b372a0df28af4e2ab41654bbcb99a2f4beeec4ed
Mauro <maurodefalco96@gmail.com>
2025-07-27 20:16:43 +0200

    feat: add logo component and enhance register page layout

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -4,7 +4,16 @@
 </script>
 
 <template>
-  <div>
-    <h1>Hello Register</h1>
+  <div class="bg-black h-screen flex flex-col md:flex-row">
+    <div class="sidebar bg-zinc-900 md:w-[30%] flex justify-center px-8 flex-col w-full">
+      <Logo class="mb-8" />
+      <h1 class="text-white text-xl mb-4">Sign Up to NuxtNote for Free</h1>
+      <p class="text-sm">Already have an account?
+        <NuxtLink class="underline" to="/login">Login to your account</NuxtLink>
+      </p>
+    </div>
+    <div class="main bg-zinc-800 md:w-[70%] w-full">
+      <h1>Hello Register</h1>
+    </div>
   </div>
 </template>
