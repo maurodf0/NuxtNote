2148ff93f4c6847b523425f7d77a3127b5a19cdc
Mauro <maurodefalco96@gmail.com>
2025-07-28 23:22:50 +0200

    Hash password with bycriptJS

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -1,15 +1,22 @@
 // Post User API Endpoint
 
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient()
 
 export default defineEventHandler( async (event) => {
   const {email, psw} = await readBody(event);
+
+  const salt = await bcrypt.genSalt(10);
+  const pswhash = await bcrypt.hash(psw, salt);
+
+
+
   await prisma.user.create({
     data: {
       email,
-      password: psw, 
+      password: pswhash, 
     }
   });
   return {
