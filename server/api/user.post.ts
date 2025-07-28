726518e9a1407b96e1f133c15cdf6288c914bc25
Mauro <maurodefalco96@gmail.com>
2025-07-28 23:35:16 +0200

    feat: add salt column to User model and update user creation API

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -6,20 +6,27 @@ import bcrypt from 'bcryptjs';
 const prisma = new PrismaClient()
 
 export default defineEventHandler( async (event) => {
+  try {
   const {email, psw} = await readBody(event);
 
   const salt = await bcrypt.genSalt(10);
   const pswhash = await bcrypt.hash(psw, salt);
 
-
-
   await prisma.user.create({
     data: {
       email,
       password: pswhash, 
+      salt: salt
     }
   });
   return {
     message: 'User created successfully',
 } 
+  } catch (error) {
+    console.error('Error creating user:', error);
+    throw createError({
+      statusCode: 409,
+      message: 'User already exists or other error occurred',
+    })
+  }
 });
