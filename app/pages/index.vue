5d568ebe38a78ec474857cb0282f06f6db63a7be
Mauro <maurodefalco96@gmail.com>
2025-08-03 19:04:59 +0200

    feat: update layout and structure of the main page for improved user experience

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,7 +12,20 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">Hello World</h1>
+  <div class="bg-zinc-900 h-screen flex flex-col md:flex-row">
+
+    <div class="sidebar bg-black md:w-[25%] flex justify-center px-8 flex-col w-full">
+      <Logo class="mb-8" />
+      <h1 class="text-white text-xl mb-4">Write your Note everywhere</h1>
+      <p class="text-sm">Don't have an account?
+        <NuxtLink class="underline" to="/register">Register now for free</NuxtLink>
+      </p>
+
+
+
+    </div>
+    <div class="main bg-zinc-800 md:w-[75%] w-full">
+      <h1>Main</h1>
+    </div>
   </div>
 </template>
