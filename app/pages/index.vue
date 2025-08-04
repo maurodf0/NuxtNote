1bb2ce304a400951204866a5919bb7ce8e3500a4
Mauro <maurodefalco96@gmail.com>
2025-08-04 23:12:59 +0200

    fix: enhance layout of notes section for improved structure and readability

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,27 +14,55 @@ definePageMeta({
 <template>
   <div class="bg-zinc-900 h-screen flex flex-col md:flex-row">
 
-    <div class="sidebar bg-black md:w-[25%] flex justify-center px-8 flex-col w-full">
+    <div class="sidebar bg-black md:w-[25%] flex p-8 flex-col w-full">
+
       <Logo class="mb-8" />
       <h1 class="text-white text-xl mb-4">Write your Note everywhere</h1>
+
+      <div class="today-notes">
       <p class="text-xs color-zinc-800">Today</p>
-      <div class="notes-wrapper mt-2 pl-2">
+        <div class="notes-wrapper today-container mt-2 pl-2">
 
-        <div class="single-note flex flex-col gap-1 p-2">
-          <h3 class="text-sm font-bold text-white">Single note for today</h3>
-          <div class="meta flex gap-4 text-xs"> 
-            <span class="text-white">Today</span>
-            <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
+          <div class="single-note flex flex-col gap-1 p-2">
+            <h3 class="text-sm font-bold text-white">Single note for today</h3>
+            <div class="meta flex gap-4 text-xs"> 
+              <span class="text-white">Today</span>
+              <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
+          </div>
+
+                  <div class="single-note bg-[#a1842c] rounded-lg flex flex-col gap-1 p-2">
+            <h3 class="text-sm font-bold text-white">Single note for today</h3>
+            <div class="meta flex gap-4 text-xs"> 
+              <span class="text-white">Today</span>
+              <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
+          </div>
+
         </div>
-
-                <div class="single-note bg-[#a1842c] rounded-lg flex flex-col gap-1 p-2">
-          <h3 class="text-sm font-bold text-white">Single note for today</h3>
-          <div class="meta flex gap-4 text-xs"> 
-            <span class="text-white">Today</span>
-            <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
-        </div>
-
       </div>
+
+
+            <div class="oldest-notes mt-6">
+      <p class="text-xs color-zinc-800">Yesterday</p>
+        <div class="notes-wrapper today-container mt-2 pl-2">
+
+          <div class="single-note flex flex-col gap-1 p-2">
+            <h3 class="text-sm font-bold text-white">Single note for today</h3>
+            <div class="meta flex gap-4 text-xs"> 
+              <span class="text-white">Yesterday</span>
+              <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
+          </div>
+
+            <div class="single-note flex flex-col gap-1 p-2">
+            <h3 class="text-sm font-bold text-white">Single note for today</h3>
+            <div class="meta flex gap-4 text-xs"> 
+              <span class="text-white">Yesterday</span>
+              <p>Lorem ipsum dolor sit amet sentiam ki....</p></div>
+          </div>
+
+        </div>
+      </div>
+
+      
 
 
     </div>
