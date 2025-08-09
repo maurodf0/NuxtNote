cc1445699ffc8e8105c6b63e52d49a96ae68a789
Mauro <maurodefalco96@gmail.com>
2025-08-09 12:49:35 +0200

    associate users to their notes

--- server/api/notes.ts
+++ server/api/notes.ts
@@ -1,4 +1,5 @@
   import { PrismaClient } from '@prisma/client'
+  import jwt from 'jsonwebtoken';
 
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient();
@@ -11,7 +12,21 @@ interface Notes {
 }
 
 try{
+  const cookies = useCookies(event)
+  const token = cookies.NuxtNoteJWT;
+
+  if (!token) {
+    throw createError({
+      statusCode: 401,
+      message: 'Unauthorized, login required',
+    });
+  }
+
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET) as string;
   const notes: Notes[] = await prisma.note.findMany({
+    where: {
+      userId: decodedToken.id,
+    }
   });
   return notes;   
 
