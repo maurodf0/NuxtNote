0c2ca292be36bf7fba932dbf68548b4390ad5458
Mauro <maurodefalco96@gmail.com>
2025-08-12 23:34:17 +0200

    prsima only on server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    ssr: {
+      noExternal: ['@prisma/client']
+    }
+  },
+  nitro: {
+    externals: {
+      inline: ['@prisma/client']
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
